b5d41e62c2eb7b476c05497e822bb3918f51fe05
Thomas Baer <thomas.baer@tbaer.eu>
2015-01-23 12:45:13 +0100

    Changes from live

--- rapi/readnfc.py
+++ rapi/readnfc.py
@@ -9,8 +9,8 @@ import nxppy
 import RPi.GPIO as GPIO
 
 
-server = 'sldev.fritz.box'
-uri = '/info.php?uid='
+server = 'sldev.solongo.office'
+uri = '/timr/g.php?uid='
 GREEN = 47
 RED = 35
 ON = GPIO.HIGH
@@ -39,31 +39,32 @@ GPIO.output(GREEN, OFF)
 
 mifare = nxppy.Mifare()
 uid1 = ''
-uid2 = ''
 while True:
     blinkled(RED, 0.1)
     time.sleep(1)
     try:
         uid1 = mifare.select()
-        blinkboth(0.2)
-        if uid1 != uid2 and uid1 is not None:
+        if uid1 is not None:
             conn = httplib.HTTPConnection(server, 80, timeout=5)
             conn.request("GET", uri + uid1)
             r = conn.getresponse()
-            if (r.status == 200):
+            if r.status == 200:
                 blinkled(GREEN, 1)
-                if re.match('\d', r.reason) is not None:
-                    for i in range(0, r.reason):
-                        blinkled(RED, 0.2)
+                ret = r.getheader('X-Return')
+                print(ret)
+                if re.match('\d', ret) is not None:
+                    for i in range(0, int(ret)):
+                        blinkled(GREEN, 0.2)
+                        time.sleep(0.2)
+                else:
+                    for i in range(0, 10):
+                        blinkled(GREEN, 0.2)
+                        time.sleep(0.2)
             else:
-                blinkled(RED, 0.2)
-                time.sleep(0.2)
-                blinkled(RED, 0.2)
-                time.sleep(0.2)
-                blinkled(RED, 0.2)
-                time.sleep(0.2)
+                for i in range(0, 3):
+                    blinkled(RED, 0.2)
+                    time.sleep(0.2)
             conn.close()
             time.sleep(5)
-        uid2 = uid1
     except nxppy.SelectError:
         pass
