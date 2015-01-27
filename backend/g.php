a371935d841a79e35158257e3d1ccf7694dab870
Thomas Uhlmann <thomasuhlmann@sl-martin.solongo.office>
2015-01-27 16:36:44 +0100

    Make GET Request to API

--- backend/g.php
+++ backend/g.php
@@ -5,5 +5,7 @@
 	$db->exec("INSERT INTO checkpoints (uid, timestamp) VALUES ('".$uid."', DATETIME('now'))");
 	$db->close();
 
+	$response = http_get("http://timr.solongo.office/timr/timetracker/web/api/cardreader?id=" . $uid);
+
 	header("Status: 200 1", true, 200);
 ?>
